c80b3cd7ed2938d1ae266895ef0ed169042735be
Mauricio Lauffer <mauriciolauffer@outlook.com>
2017-02-06 19:21:48 +1100

    [FEATURE] Use Fetch

--- routes/bot.js
+++ routes/bot.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -21,11 +21,10 @@ router.get('/webhook', (req, res) => {
 router.post('/webhook', (req, res) => {
   // Make sure this is a page subscription
   if (req.body.object === "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
+    // Iterate over each entry there may be multiple entries if batched
+    req.body.entry.forEach(function (entry) {
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
+      entry.messaging.forEach(function (event) {
         if (event.postback) {
           processPostback(event);
         } else if (event.message) {
@@ -41,8 +40,8 @@ router.post('/webhook', (req, res) => {
 
 function processMessage(event) {
   if (!event.message.is_echo) {
-    var message = event.message;
-    var senderId = event.sender.id;
+    let message = event.message;
+    let senderId = event.sender.id;
 
     console.log("Received message from senderId: " + senderId);
     console.log("Message is: " + JSON.stringify(message));
@@ -51,27 +50,27 @@ function processMessage(event) {
 
     // You may get a text or attachment but not both
     /*if (message.text) {
-      var formattedMsg = message.text.toLowerCase().trim();
+     var formattedMsg = message.text.toLowerCase().trim();
 
-      // If we receive a text message, check to see if it matches any special
-      // keywords and send back the corresponding movie detail.
-      // Otherwise, search for new movie.
-      switch (formattedMsg) {
-        case "plot":
-        case "date":
-        case "runtime":
-        case "director":
-        case "cast":
-        case "rating":
-          getMovieDetail(senderId, formattedMsg);
-          break;
+     // If we receive a text message, check to see if it matches any special
+     // keywords and send back the corresponding movie detail.
+     // Otherwise, search for new movie.
+     switch (formattedMsg) {
+     case "plot":
+     case "date":
+     case "runtime":
+     case "director":
+     case "cast":
+     case "rating":
+     getMovieDetail(senderId, formattedMsg);
+     break;
 
-        default:
-          findMovie(senderId, formattedMsg);
-      }
-    } else if (message.attachments) {
-      sendMessage(senderId, {text: "Sorry, I don't understand your request."});
-    }*/
+     default:
+     findMovie(senderId, formattedMsg);
+     }
+     } else if (message.attachments) {
+     sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+     }*/
   }
 }
 
@@ -80,27 +79,23 @@ function processPostback(event) {
   let payload = event.postback.payload;
 
   if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      let greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        let bodyObj = JSON.parse(body);
-        let name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      let message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
-    });
+    // Get user's first name from the User Profile API and include it in the greeting
+    fetch('https://graph.facebook.com/v2.6/' + senderId + '?access_token=process.env.PAGE_ACCESS_TOKEN&fields=first_name')
+      .then(function (res) {
+        let greeting = "";
+        if (!res.ok) {
+          console.log("Error getting user's name: " + res.statusText);
+        } else {
+          let body = JSON.parse(body);
+          let name = body.first_name;
+          greeting = "Hi " + name + ". ";
+        }
+        let message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+        sendMessage(senderId, {text: message});
+      })
+      .catch((res) => {
+        console.log("Error sending message: " + response.statusText);
+      });
   } else {
     sendMessage(senderId, {text: 'Generic message for test'});
   }
@@ -108,19 +103,21 @@ function processPostback(event) {
 
 // sends message to user
 function sendMessage(recipientId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
+  fetch('https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN, {
+    method: 'POST',
+    body: JSON.stringify({
       recipient: {id: recipientId},
       message: message,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
+    })
+  })
+    .then((res) => {
+      if (!res.ok) {
+        console.log("Error sending message: " + response.statusText);
+      }
+    })
+    .catch((res) => {
+      console.log("Error sending message: " + response.statusText);
+    });
 }
 
 module.exports = router;
