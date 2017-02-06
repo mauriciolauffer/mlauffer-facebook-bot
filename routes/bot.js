d9490fd5c967bd59bff2c133815f51cc3b67c7f6
Mauricio Lauffer <mauriciolauffer@outlook.com>
2017-02-06 22:05:21 +1100

    [FEATURE] Search country

--- routes/bot.js
+++ routes/bot.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const fetch = require('node-fetch');
+const request = require('request');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -9,19 +9,20 @@ router.get('/', (req, res) => {
 });
 
 router.get('/webhook', (req, res) => {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
+  if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
+    console.log('Verified webhook');
+    res.status(200).send(req.query['hub.challenge']);
   } else {
-    console.error("Verification failed. The tokens do not match.");
+    console.error('Verification failed. The tokens do not match.');
     res.status(403).send('Verification failed. The tokens do not match.');
   }
 });
 
 router.post('/webhook', (req, res) => {
   // Make sure this is a page subscription
-  if (req.body.object === "page") {
-    // Iterate over each entry there may be multiple entries if batched
+  if (req.body.object === 'page') {
+    // Iterate over each entry
+    // There may be multiple entries if batched
     req.body.entry.forEach(function (entry) {
       // Iterate over each messaging event
       entry.messaging.forEach(function (event) {
@@ -43,81 +44,172 @@ function processMessage(event) {
     let message = event.message;
     let senderId = event.sender.id;
 
-    console.log("Received message from senderId: " + senderId);
-    console.log("Message is: " + JSON.stringify(message));
-
-    sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+    console.log('Received message from senderId: ' + senderId);
+    console.log('Message is: ' + JSON.stringify(message));
 
     // You may get a text or attachment but not both
-    /*if (message.text) {
-     var formattedMsg = message.text.toLowerCase().trim();
+    if (message.text) {
+      let formattedMsg = message.text.toLowerCase().trim();
+      switch (formattedMsg) {
+        case 'mapa':
+        case 'capital':
+        case 'moeda':
+        case 'população':
+          getCountryDetail(senderId, formattedMsg);
+          break;
 
-     // If we receive a text message, check to see if it matches any special
-     // keywords and send back the corresponding movie detail.
-     // Otherwise, search for new movie.
-     switch (formattedMsg) {
-     case "plot":
-     case "date":
-     case "runtime":
-     case "director":
-     case "cast":
-     case "rating":
-     getMovieDetail(senderId, formattedMsg);
-     break;
-
-     default:
-     findMovie(senderId, formattedMsg);
-     }
-     } else if (message.attachments) {
-     sendMessage(senderId, {text: "Sorry, I don't understand your request."});
-     }*/
+        default:
+          findCountry(senderId, formattedMsg);
+      }
+    } else if (message.attachments) {
+      sendMessage(senderId, {text: 'Desculpe, Não entendi sua requisição...'});
+    }
   }
 }
 
 function processPostback(event) {
   let senderId = event.sender.id;
   let payload = event.postback.payload;
-
-  if (payload === "Greeting") {
+  if (payload === 'Greeting') {
     // Get user's first name from the User Profile API and include it in the greeting
-    fetch('https://graph.facebook.com/v2.6/' + senderId + '?access_token=process.env.PAGE_ACCESS_TOKEN&fields=first_name')
-      .then(function (res) {
-        let greeting = "";
-        if (!res.ok) {
-          console.log("Error getting user's name: " + res.statusText);
-        } else {
-          let body = JSON.parse(body);
-          let name = body.first_name;
-          greeting = "Hi " + name + ". ";
-        }
-        let message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-        sendMessage(senderId, {text: message});
-      })
-      .catch((res) => {
-        console.log("Error sending message: " + response.statusText);
-      });
-  } else {
-    sendMessage(senderId, {text: 'Generic message for test'});
+    request({
+      url: 'https://graph.facebook.com/v2.6/' + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: 'first_name'
+      },
+      method: 'GET'
+    }, function (error, response, body) {
+      let greeting;
+      if (error) {
+        console.log("Error getting user's name: " + error);
+      } else {
+        let bodyObj = JSON.parse(body);
+        let name = bodyObj.first_name;
+        greeting = 'Olá ' + name + '. ';
+      }
+      let message = greeting + 'Meu nome é TripBot, eu sou um robô. Eu posso te falar informações sobre alguns países. Qual país você gostaria de conhecer?';
+      sendMessage(senderId, {text: message});
+    });
+  } else if (payload === 'Correto') {
+    sendMessage(senderId, {text: "Sweet! O que você gostaria de ver? Digite: 'mapa', 'capital', 'moeda', 'população' para mais detalhes."});
+  } else if (payload === 'Incorreto') {
+    sendMessage(senderId, {text: 'Oops! Deu zica! Tente digitar o nome do país corretamente  =)'});
   }
 }
 
 // sends message to user
 function sendMessage(recipientId, message) {
-  fetch('https://graph.facebook.com/v2.6/me/messages?access_token=' + process.env.PAGE_ACCESS_TOKEN, {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
     method: 'POST',
-    body: JSON.stringify({
+    json: {
       recipient: {id: recipientId},
       message: message,
-    })
-  })
-    .then((res) => {
-      if (!res.ok) {
-        console.log("Error sending message: " + response.statusText);
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ' + response.error);
+    }
+  });
+}
+
+function getCountryDetail(userId, field) {
+  request('https://restcountries.eu/rest/v1/name/' + countryName, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let message;
+      let countries = JSON.parse(body);
+      let country = countries[0];
+      if (country) {
+        message = {
+          attachment: {
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: [{
+                title: country.name.concat('(', country.nativeName, ')')
+              }]
+            }
+          }
+        };
+        switch (formattedMsg) {
+          case 'mapa':
+            let mapUrl = 'https://www.google.com.au/maps/';
+            if (country.latlng.length) {
+              mapUrl = mapUrl.concat('/search/', country.name);
+            } else {
+              mapUrl = mapUrl.concat('@', country.latlng[0], ',', country.latlng[1], ',7z');
+            }
+            message.attachment.payload.elements[0].subtitle = 'Veja o mapa do país';
+            message.attachment.payload.elements[0].buttons = [{
+              type: 'web_url',
+              title: 'Google Maps'
+            }];
+            break;
+          case 'capital':
+            message.attachment.payload.elements[0].subtitle = 'Capital: ' + country.capital;
+            break;
+          case 'moeda':
+            message.attachment.payload.elements[0].subtitle = 'Moeda: ' + country.currencies[0];
+            break;
+          case 'população':
+            message.attachment.payload.elements[0].subtitle = 'População: ' + country.population;
+            break;
+
+          default:
+            break;
+        }
+        sendMessage(userId, message);
+
+      } else {
+        console.log('Oops! Deu algum erro por aqui...');
+        sendMessage(userId, {text: 'Oops! Deu algum erro por aqui...'});
       }
-    })
-    .catch((res) => {
-      console.log("Error sending message: " + response.statusText);
-    });
+    } else {
+      sendMessage(userId, {text: 'Oops! Deu algum erro por aqui...'});
+    }
+  });
+}
+
+function findCountry(userId, countryName) {
+  request('https://restcountries.eu/rest/v1/name/' + countryName, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let countries = JSON.parse(body);
+      let country = countries[0];
+      if (country) {
+        let message = {
+          attachment: {
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: [{
+                title: country.name + '(' + country.nativeName + ')',
+                subtitle: 'Este é o país que você está procurando?',
+                image_url: 'http://www.geognos.com/api/en/countries/flag/' + country.alpha2Code + '.png',
+                buttons: [{
+                  type: 'postback',
+                  title: 'Sim',
+                  payload: 'Correto'
+                }, {
+                  type: 'postback',
+                  title: 'Não',
+                  payload: 'Incorreto'
+                }]
+              }]
+            }
+          }
+        };
+        sendMessage(userId, message);
+
+      } else {
+        console.log('Oops! Deu algum erro por aqui...');
+        sendMessage(userId, {text: 'Oops! Deu algum erro por aqui...'});
+      }
+    } else {
+      sendMessage(userId, {text: 'Oops! Deu algum erro por aqui...'});
+    }
+  });
 }
 
 module.exports = router;
