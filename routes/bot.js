f114a5a3071cb469eb48d06c2a1a5e9346bad77b
Mauricio Lauffer <mauricio.lauffer@uxcoxygen.com>
2017-02-06 16:32:39 +1100

    [FEATURE] Send message

--- routes/bot.js
+++ routes/bot.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -17,103 +18,66 @@ router.get('/webhook', (req, res) => {
   }
 });
 
-app.post('/webhook', (req, res) => {
+router.post('/webhook', (req, res) => {
   // Make sure this is a page subscription
-  if (req.body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    req.body.entry.forEach((entry) => {
-      let pageID = entry.id;
-      let timeOfEvent = entry.time;
-
+  if (req.body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+    req.body.entry.forEach(function(entry) {
       // Iterate over each messaging event
-      entry.messaging.forEach((event) => {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
         }
       });
     });
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.status(200).send();
+    res.sendStatus(200);
   }
 });
 
+function processPostback(event) {
+  let senderId = event.sender.id;
+  let payload = event.postback.payload;
 
-
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      let greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        let bodyObj = JSON.parse(body);
+        let name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      let message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
   }
 }
 
-function sendGenericMessage(recipientId, messageText) {
-  // To be expanded in later sections
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
+// sends message to user
+function sendMessage(recipientId, message) {
   request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
     }
   });
 }
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-
-router.use('/', require('./bot'));
-
-module.exports = router;
