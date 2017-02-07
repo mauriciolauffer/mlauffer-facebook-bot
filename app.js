d8dfe13402efb93328eedf1b4799b810ec3646ba
Mauricio Lauffer <mauricio.lauffer@uxcoxygen.com>
2017-02-07 11:30:02 +1100

    [FIX] Remove default error handler

--- app.js
+++ app.js
@@ -20,30 +20,6 @@ app.use(helmet());
 app.use(morgan('combined'));
 app.use('/', require('./routes/bot'));
 
-// Development error handler will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    const errorResponse = {
-      message: err.message,
-      messageCode: err.messageCode,
-      error: err.validationErrors || err
-    };
-    console.info('Error: ' + err.status);
-    res.status(err.status || 500).json(errorResponse);
-  });
-}
-
-// Production error handler no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  const errorResponse = {
-    message: err.message,
-    messageCode: err.messageCode,
-    error: err.validationErrors || {}
-  };
-  console.info('Error: ' + err.status);
-  res.status(err.status || 500).json(errorResponse);
-});
-
 if (cluster.isMaster) {
   // Fork workers
   for (let i = 0; i < numCPUs; i++) {
